47f9a4782eb7a90593e1b753f16474c13317d14a
Zahran <zahranrauhillahraziq22435@gmail.com>
2025-10-06 14:41:32 +0700

    update

---
+++ modul4/app/Http/Controllers/StudentController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = [
+            [
+                'name' => 'Rohman',
+                'major' => 'Informatika',
+                'age' => 22,
+                'courses' => ['Pemrograman Web', 'Database', 'Cloud Computing'],
+            ],
+            [
+                'name' => 'Siti',
+                'major' => 'Sistem Informasi',
+                'age' => 21,
+                'courses' => ['UI/UX Design', 'Manajemen Proyek', 'IoT'],
+            ],
+        ];
+
+        return view('students.index', compact('students'));
+    }
+}
+
+?>
